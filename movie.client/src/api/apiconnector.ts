f6f635690608748343b149ca03730121bb88ce3a
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-07-24 11:43:16 +0200

    Adding Semantic UI

--- movie.client/src/api/apiconnector.ts
+++ movie.client/src/api/apiconnector.ts
@@ -1,6 +1,7 @@
 import axios, { Axios, AxiosResponse } from "axios";
 import { MovieDto } from "../models/movieDto";
 import { GetMoviesResponse } from "../models/getMoviesResponse";
+import { GetMovieByIdResponse } from "../models/getMovieByIdResponse";
 
 
 const API_BASE_URL = 'http://localhost:5136';
@@ -30,5 +31,37 @@ const apiConnector = {
             console.log(error);
             throw error;
         }
+    },
+
+    editMovie: async (movie: MovieDto): Promise<void> => {
+        try{
+            await axios.put<number>(`${API_BASE_URL}/movies`, movie);
+        }
+        catch(error){
+            console.log(error);
+            throw error;
+        }
+    },
+
+    deleteMovie: async (movieId: string): Promise<void> => {
+        try{
+            await axios.delete<number>(`${API_BASE_URL}/movies/${movieId}`);
+        }
+        catch(error){
+            console.log(error);
+            throw error;
+        }
+    },
+
+    getMovieById: async (movieId: string): Promise<MovieDto | undefined> => {
+       try{
+            const response = await axios.get<GetMovieByIdResponse>(`${API_BASE_URL}/movies/${movieId}`);
+            return response.data.movieDto;
+       }catch(error){
+            console.log(error);
+            throw error;
+        }
     }
 }
+
+export default apiConnector;
--- movie.client/src/components/MovieTable.tsx
+++ movie.client/src/components/MovieTable.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react"
+import { MovieDto } from "../models/movieDto"
+import apiConnector from "../api/apiConnector";
+import { Container } from "semantic-ui-react";
 
 
 export default function MovieTable(){
 
+    const [movies, setMovies] = useState<MovieDto[]>([]);
+
+    useEffect(() =>{
+        const fetchMovies = async () => {
+            const fetchedMovies = await apiConnector.getMovies();
+            setMovies(fetchedMovies);
+        }
+        fetchMovies();
+    }, []);
+
     return(
 
         <>
-        
+           
         </>
     )
 }
